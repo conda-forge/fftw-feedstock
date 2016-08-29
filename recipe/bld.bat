a4279684ec5f8767dad34da24f30720d89fae7e7
shadow_walker <shadowwalkersb@gmail.com>
2016-08-28 22:15:20 -0500

    cleanup

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -17,15 +17,6 @@ copy libfftw3-3.lib "%LIBRARY_LIB%\"
 copy libfftw3f-3.lib "%LIBRARY_LIB%\"
 copy libfftw3l-3.lib "%LIBRARY_LIB%\"
 
-:: make .lib with easier name so that cmake can find it
-copy libfftw3-3.lib "%LIBRARY_LIB%\fftw3.lib"
-
 copy *.h "%LIBRARY_INC%\"
 
 if errorlevel 1 exit 1
-
-:: Add more build steps here, if they are necessary.
-
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
