950f34e508e1c8d11a4c6b0eeb66d545ad2a8106
shadow_walker <shadowwalkersb@gmail.com>
2016-08-28 22:26:56 -0500

    add comments and use regex to copy files

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,14 +9,13 @@ lib /MACHINE:%MACHINE% /def:libfftw3-3.def
 lib /MACHINE:%MACHINE% /def:libfftw3f-3.def
 lib /MACHINE:%MACHINE% /def:libfftw3l-3.def
 
-copy libfftw3-3.dll "%LIBRARY_LIB%\"
-copy libfftw3f-3.dll "%LIBRARY_LIB%\"
-copy libfftw3l-3.dll "%LIBRARY_LIB%\"
+:: copy DLLs to LIBRARY_BIN
+copy libfftw3-3*.dll "%LIBRARY_BIN%\"
 
-copy libfftw3-3.lib "%LIBRARY_LIB%\"
-copy libfftw3f-3.lib "%LIBRARY_LIB%\"
-copy libfftw3l-3.lib "%LIBRARY_LIB%\"
+:: copy .lib "import libraries" to LIBRARY_LIB
+copy libfftw3-3*.lib "%LIBRARY_LIB%\"
 
+:: copy headers to LIBRARY_INC
 copy *.h "%LIBRARY_INC%\"
 
 if errorlevel 1 exit 1
