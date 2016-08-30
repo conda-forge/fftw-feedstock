039da30315da9c4434f63c23c4845574eb9fefc0
shadow_walker <shadowwalkersb@gmail.com>
2016-08-30 18:35:54 -0500

    find msbuild

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,11 +4,6 @@ if "%ARCH%" == "64" (
   set MACHINE=X86
 )
 
-xcopy /s %RECIPE_DIR%\\win_sln .
-
-cd win32
-msbuild.exe fftw-vs2008.sln /t:libfftw
-msbuild.exe fftw-vs2008.sln /t:libfftwf
-msbuild.exe fftw-vs2008.sln /t:libfftwl
+where /r c:\ msbuild
 
 if errorlevel 1 exit 1
